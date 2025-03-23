118cc0fab497effea9dc76ee34aea692ddd383c5
Animesh420 <ani420mkjee@gmail.com>
2025-03-23 20:24:05 +0530

    Adding tokens

---
+++ monkey/token/token.go
@@ -0,0 +1,36 @@
+// sudo rm -rf /usr/local/go && tar -C /usr/local -xzf ~/go1.24.1.linux-amd64.tar.gz
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLLEGAL"
+	EOF     = "EOF"
+
+	//Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y..
+	INT   = "INT"   // 134534
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
